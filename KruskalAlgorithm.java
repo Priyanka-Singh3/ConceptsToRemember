6adc03b0017888b68c48fb1b2cc2c07df6139e79
Priyanka Singh <pri1062003@gmail.com>
2025-01-26 00:13:25 +0530

    Kruskal Algorithm

---
+++ KruskalAlgorithm.java
@@ -0,0 +1,43 @@
+import java.utils.DisjointSet;
+import java.util.*;
+
+public class KruskalAlgorithm {
+
+    class Edge {
+        int wt;
+        int u;
+        int v;
+
+        public Edge(int wt, int u, int v) {
+            this.wt = wt;
+            this.u = u;
+            this.v = v;
+        }
+    }
+
+    public int minimumSpanningTree(ArrayList<Edge> edges, int n) {
+        DisjointSet ds = new DisjointSet(n);
+        for(Edge e: edges) {
+            int wt = e.wt;
+            int u = e.u;
+            int v = e.v;
+        }
+
+        Collections.sort(edges, (a, b) -> {
+            return Integer.compare(a.wt, b.wt);
+        });
+
+        int mst = 0;
+        for(Edge e: edges) {
+            int wt = e.wt;
+            int u = e.u;
+            int v = e.v;
+
+            if(ds.findUPar(u) != ds.findUPar(v)) {
+                mst += wt;
+                ds.unionBySize(u, v);
+            }
+        }
+        return mst;
+    }
+}
