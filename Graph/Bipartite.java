4572f36d0df48fb6e7c186af1acddf4a4b9c53ed
Priyanka Singh <pri1062003@gmail.com>
2025-01-29 20:52:43 +0530

    dfs code of bipartite graph added

--- Graph/Bipartite.java
+++ Graph/Bipartite.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Bipartite {
 
     //bfs method
-    boolean isBipartite(ArrayList<Integer> adj[], int V) {
+    boolean isBipartiteBfs(ArrayList<Integer> adj[], int V) {
         int col[] = new int[V];
         Arrays.fill(col, -1);
         Queue<Integer> q = new LinkedList<>();
@@ -22,6 +22,27 @@ public class Bipartite {
                     }
                 }
             }
+        }
+        return true;
+    }
+
+    boolean isBipartiteDfs(ArrayList<Integer> adj[], int V) {
+        int col[] = new int[V];
+        Arrays.fill(col, -1);
+        for(int i = 0; i < V; i++) {
+            if(col[i] == -1) {
+                if(dfs(i, 0, col, adj) == false) return false;
+            }
+        }
+        return true;
+    }
+    boolean dfs(int node, int color, int[]col, ArrayList<Integer> adj[]) {
+        col[node] = color;
+        for(int adjNode: adj[i]) {
+            if(col[adjNode] == -1) {
+                if(dfs(adjNode, 1-color, col, adj) == false) return false;
+            }
+            else if(col[adjNode] == col[node]) return false;
         }
         return true;
     }
