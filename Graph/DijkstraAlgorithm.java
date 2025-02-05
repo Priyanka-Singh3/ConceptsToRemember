7d3c3c4554cc2ec100992863dab325835cb22207
Priyanka Singh <pri1062003@gmail.com>
2025-02-05 21:19:15 +0530

    time complexity derivation of dijkstra's algo added

--- Graph/DijkstraAlgorithm.java
+++ Graph/DijkstraAlgorithm.java
@@ -26,17 +26,21 @@ public class DijkstraAlgorithm {
     }
     public int[] dijkstra(ArrayList<Edge> graph[], int src) {
         int V = graph.length;
+
         PriorityQueue<Pair> pq = new PriorityQueue<Pair>((a, b) -> a.wt - b.wt);
         pq.add(new Pair(src, 0));
+
         int dist[] = new int[V];
         Arrays.fill(dist, Integer.MAX_VALUE);
+
         dist[src] = 0;
-        while(!pq.isEmpty()) {
-            Pair curr = pq.remove();
+
+        while(!pq.isEmpty()) {  // max V times
+            Pair curr = pq.remove(); // log(heap_size)
             int node = curr.node;
             int wt = curr.wt;
 
-            for(Edge adjNode: graph[node]) {
+            for(Edge adjNode: graph[node]) { // ne -> total edges -> one can have atmost V-1 edges
                 int u = adjNode.u;
                 int v = adjNode.v;
                 int w = adjNode.wt;
@@ -45,11 +49,19 @@ public class DijkstraAlgorithm {
 
                 if(w + wt < dist[v]) {
                     dist[v] = w + wt;
-                    pq.add(new Pair(v, dist[v]));
+                    pq.add(new Pair(v, dist[v])); // log(heap_size)
                 }
 
             }
         }
         return dist;
     }
+
+    // time complexity = E logV
+    // O(V * ({log(heap_size) + ne * log(heap_size)}))
+    // O(V * ({log(heap_size) * (ne + 1)}))
+    // O(V * ({log(V^2) * (V - 1 + 1)}))
+    // O(V^2 * log(V^2)) now total nodes are V and for one node we can have V-1 edges hence V*(V-1) = total edges = E
+    // O(E * 2logV)
+    // hence, O(E logV) is the time complexity
 }
