5bed42ebb30a5193089b959fbe6bee8f164b7c0b
Priyanka Singh <pri1062003@gmail.com>
2025-03-07 13:05:49 +0530

    gcd of a and b -> euclidean algorithm

---
+++ Maths/GCD.java
@@ -0,0 +1,14 @@
+package Maths;
+
+public class GCD {
+    public static int gcd(int a, int b) {
+        while(a > 0 && b > 0) {
+            if(a > b) a = a % b;
+            else {
+                b = b % a;
+            }
+        }
+        if(a == 0) return b;
+        else return a;
+    }
+}
